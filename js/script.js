84f96a28bdebfd73256fa6d06cf1e8b3050ef6a2
caiodds <rhyanteixeira04@gmail.com>
2023-11-02 11:28:41 -0300

    at2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //33c62e271dfad296e21e73c8dda3a11a
 //Variaveis
-const apiKey = "33c62e271dfad296e21e73c8dda3a11a";
+const apiKey = "2ac36586912c94e6d8b4db038c12ec3d";
 const apiCountryURL= "https://flagsapi.com/BE/flat/png/"
 
 
@@ -8,24 +8,32 @@ const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
-const descElement = document.querySelector("description");
+const descElement = document.querySelector("#description");
 const WeatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
 const umidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
 //Funções
 const GetWeatherData = async(city) =>{
-    const apiWeatherURL = `https://api.openweathermap.org/data/3.0/onecall?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
     const res = await fetch(apiWeatherURL)
     const data = await res.json()
 
-    console.log(data)
+    return data
 }
 
 
-const ShowWeatherData = (city) =>{
-    GetWeatherData(city)
+const ShowWeatherData = async (city) =>{
+    const data = await GetWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    WeatherIconElement.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    umidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`
 };
 
 
